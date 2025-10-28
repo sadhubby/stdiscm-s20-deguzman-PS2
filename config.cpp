97ff1ca4b5cc81e4dbcdc0a786d9d1cd8a5760e0
edg <122878019+sadhubby@users.noreply.github.com>
2025-10-29 00:38:31 +0800

    feat: added config.txt file reader

--- config.cpp
+++ config.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 
@@ -11,4 +13,26 @@ Config loadConfig(const string &fileName) {
     cerr << "Could not open" << fileName << "\n";
     exit(1);
   }
+
+  unordered_map<string, string> config;
+  string line;
+  while (getline(file, line)) {
+    istringstream iss(line);
+    string key, value;
+    if (iss >> key >> value) {
+      config[key] = value;
+    }
+  }
+  file.close();
+
+  Config cfg{};
+
+  cfg.n = stoi(config.at("n"));
+  cfg.t = stoi(config.at("t"));
+  cfg.h = stoi(config.at("h"));
+  cfg.d = stoi(config.at("t"));
+  cfg.t1 = stoi(config.at("t1"));
+  cfg.t2 = stoi(config.at("t2"));
+
+  return cfg;
 }
